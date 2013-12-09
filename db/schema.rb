c30db234eda1792d808107cd95cd4410409f1db0
Selmarien <selmarien@gmail.com>
2013-12-09 06:34:07 +0100

    premire validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131209022917) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "adminpack"
+
+  create_table "users", force: true do |t|
+    t.string   "nom"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
