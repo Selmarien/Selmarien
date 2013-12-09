22286101436ba4a1de36283173f54e96660bb29e
Selmarien <selmarien@gmail.com>
2013-12-09 16:37:11 +0100

    test 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Selmarien::Application.routes.draw do
   resources :users
+  root :to => redirect("/users")
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
